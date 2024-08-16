9a0fa4f9c4f364e67eda14a6392b1c7a74d6b557
Cherrypick14 <cherrypicklet@gmail.com>
2024-08-16 12:08:38 +0300

    Fix id to use Nat

--- src/kintsugi_backend/src/models.rs
+++ src/kintsugi_backend/src/models.rs
@@ -1,9 +1,10 @@
 use serde::{Deserialize, Serialize};
 use candid::CandidType;
+use candid::Nat;
 
 #[derive(Serialize, Deserialize, Clone, CandidType)]
 pub struct Report {
-    pub id: u64,
+    pub id: Nat,
     pub incident_type: String,
     pub description: String,
     pub date: String,
